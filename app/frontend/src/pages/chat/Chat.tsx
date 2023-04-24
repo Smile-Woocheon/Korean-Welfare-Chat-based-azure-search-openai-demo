606273da4ead68bb3e6176c92be1aa0d26df71f6
Smile-Woocheon <130344957+Smile-Woocheon@users.noreply.github.com>
2023-04-25 06:09:19 +0900

    안내문 연결

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -135,9 +135,14 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>사회복지 데이터 기반으로 대화하세요</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>사전에 학습한 매뉴얼, 사례집 등을 기반으로 대답합니다.</h2>
-                            <h3 className={styles.chatEmptyStateSubtitle}>문의 : 신용우</h3>
+                            <h1 className={styles.chatEmptyStateTitle}>사회복지 데이터 Chat</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}>사전에 학습한 매뉴얼, 사례집 등을 기반으로 대답합니다. 문의 : 신용우</h2>
+                            <h3 className={styles.chatEmptyStateSubtitle}>
+                                <a href="https://sites.google.com/woocheon.or.kr/ai-chat/intro" target="_blank">
+                                    상세 안내 및 참여하기 클릭
+                                </a>
+                            </h3>
+
                             <ExampleList onExampleClicked={onExampleClicked} />
                         </div>
                     ) : (
